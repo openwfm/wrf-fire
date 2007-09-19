acea5e9e63a49646194cd5892e1a89da104eef00
Jan Mandel <Jan.Mandel@gmail.com>
2007-09-19 11:19:50 -0600

    works up to model level except propagation stops at some point...

---
+++ wrfv2_fire/phys/prop_ls.m
@@ -0,0 +1,86 @@
+function [t,phi]=prop_ls(phi0,time0,time1,vx,vy,r,dx,dy,spread_rate)
+% a jm/mkim version of propagate
+% follows Osher-Fedkiw and Mitchell toolbox
+
+% phi       level function out
+% phi0      level function in
+% time0     starting time
+% time1     end time
+% vx        component x of velocity field, passed to spread_rate
+% vy        component y of velocity field, passed to spread_rate
+% r         propagation speed in normal direction
+% dx        mesh step in x direction
+% dy        mesh step in y direction
+% 
+
+% initialize
+phi=phi0; % allocate level function
+[m,n]=size(phi);
+t=time0;  % current time
+tol=300*eps;
+istep=0;
+msteps=1000;
+diffLx=zeros(m,n);
+diffLy=zeros(m,n);
+diffRx=zeros(m,n);
+diffRy=zeros(m,n);
+
+% time loop
+while t<time1-tol & istep < msteps
+    istep=istep+1;
+    % one-sided differences
+    [diffLx,diffRx,diffLy,diffRy,diffCx,diffCy]=get_diff(phi,dx,dy);         
+    % gradient of level function is the normal direction
+    scale=sqrt(diffCx.*diffCx+diffCy.*diffCy+eps); 
+    nvx=diffCx./scale;
+    nvy=diffCy./scale;
+    % propagation speed 
+    speed=spread_rate(r,vx,vy,nvx,nvy,scale);
+    speed=max(speed,0);
+    % to recover advection vv and spread r, transition between:
+    % r = 0 & (normal,v) > const*speed => vv=speed*v/(normal,v) & rr=0
+    % speed >> (normal,v) => vv=0 & rr=speed
+    nvv = nvx .* vx + nvy .* vy;
+    a=2*nvv>speed;
+    nvv(a==0)=1;
+    %no_wind_cases=sum(sum(1-a))
+    rr=speed.*(1-a);
+    vvx = vx .* a .* speed ./ nvv;
+    vvy = vy .* a .* speed ./ nvv;
+    % vvx=vx;vvy=vy;
+    % Godunov scheme for normal motion
+    flowLx=(diffLx>=0 & diffCx>=0);
+    flowRx=(diffRx<=0 & diffCx<0);
+    diff2x=diffLx.*flowLx + diffRx.*flowRx; %
+    flowLy=(diffLy>=0 & diffCy>=0);
+    flowRy=(diffRy<=0 & diffCy<0);
+    diff2y=diffLy.*flowLy + diffRy.*flowRy; %
+    grad=sqrt(diff2x.*diff2x + diff2y.*diff2y);
+    nz=find(grad);
+    %tbound_n(1)=max(abs(r.*sqrt(diff2x(nz)))./grad(nz))/dx; % time step bnd
+    %tbound_n(2)=max(abs(r.*sqrt(diff2y(nz)))./grad(nz))/dy;
+    tbound_np=r.*(abs(diff2x)./dx+abs(diff2y)./dy); % pointwise time step bnd
+    tbound_n=max(tbound_np(nz)./abs(grad(nz))); % worst case
+    tend_n =-rr.*grad;   % the result, tendency
+    % standard upwinding for advection
+    tend_a=-(diffLx.*max(vvx,0)+diffRx.*min(vvx,0)+...
+        diffLy.*max(vvy,0)+diffRy.*min(vvy,0));
+    tbound_ax=max(abs(vvx(:)))/dx;
+    tbound_ay=max(abs(vvy(:)))/dy;
+    % complete right-hand side
+    tend=tend_n + tend_a;
+    % complete time step bound
+    tbound = 1/(tbound_n+tbound_ax+tbound_ay+eps); 
+    % decide on timestep
+    dt=min(time1-t,0.5*tbound);
+    % trailing edge correction - do not allow fireline to go backwards
+    tt=max(dx,dy);
+    ins=find(phi<=-0);
+    tend(ins)=min(tend(ins),0);
+    % advance
+    phi=phi+dt*tend;
+    t=t+dt;
+end
+fprintf('prop_ls: %g steps from %g to %g\n',istep,time0,t)
+end % of the function prop_ls
+
---
+++ wrfv2_fire/phys/prop_test.m
@@ -0,0 +1,204 @@
+function prop_test
+% a jm/mkim version of propagate
+% follows Osher-Fedkiw and Mitchell toolbox
+
+% defaults
+% grid
+xs=10; % domain size
+ys=10;
+m=101 % domain mesh
+n=101
+time0=0
+time1=10
+r=0
+speed=2  % wind speed
+wind='c'
+
+example='g'
+
+switch example
+    case {'circlespin','c'}
+    wind='c'
+    ignition='c'
+    case {'gostraight','g'}
+    wind='s'
+    ignition='c'
+    case {'windmill','w'}
+    time1=3;
+    r=0.1;
+    wind='c'
+    ignition='w'    
+    case {'spinstardemo','s'},
+    % same as spinStarDemo('low',1)
+    xs=2; % domain size
+    ys=2;
+    m=101 % domain mesh
+    n=101
+    % normal propagation speed
+    r=0.2;
+    % max wind speed
+    w=1;
+    wind='c'
+    ignition='s'
+    time0=0
+    time1=1
+    case {'o','only growth'}
+    speed=0
+    wind='s'
+    ignition='c'
+    r=0.1
+end
+
+phi=zeros(m,n); % allocate level function
+
+dx=xs/(m-1);
+dy=ys/(n-1);
+x=dx*[0:m-1];
+y=dy*[0:n-1];
+
+switch wind
+    case {'straight','s'}
+        alpha=-1.3*pi
+        vx=speed*cos(alpha)*ones(m,n);
+        vy=speed*sin(alpha)*ones(m,n);
+    case {'circular','c'}
+        [xx,yy]=ndgrid(x-xs/2,y-ys/2);
+        rotation=-0.75 * pi;
+        rotation=0.3 * pi;
+        vx=yy*rotation;
+        vy=-xx*rotation;
+    otherwise
+        error('no wind')
+end
+
+switch ignition
+    case {'star','s'}
+        points = 7;
+        shift = 2.5;
+        scale = 0.20;
+        [xx,yy]=ndgrid(x-xs/2,y-ys/2);
+        [ theta, rad ] = cart2pol(xx, yy);
+        phi = rad - scale * (cos(points * theta) + shift);
+    case {'circle','c'}
+        c=[0.8*xs,0.5*ys]; % center
+        d=0.5;      % radius
+        for i=1:m
+            for j=1:n
+%               phi(i,j)=sign(norm([i*dx,j*dy]-c)-d);
+                phi(i,j)=(norm([i*dx,j*dy]-c)-d);
+            end
+        end
+    case {'widmill','w'}
+        phi=ones(m,n);
+        phi(floor(m*0.20):ceil(m*0.45),floor(n*0.49):ceil(n*0.51))=-1;
+        phi(floor(m*0.55):ceil(m*0.80),floor(n*0.49):ceil(n*0.51))=-1;
+        phi(floor(m*0.49):ceil(m*0.51),floor(n*0.20):ceil(n*0.45))=-1;
+        phi(floor(m*0.49):ceil(m*0.51),floor(n*0.55):ceil(n*0.80))=-1;
+    otherwise
+        error('no ignition')
+end
+
+plotSteps = 50;               % How many intermediate plots to produce?
+t0 = time0;                      % Start time.
+
+% Period at which intermediate plots should be produced.
+tPlot = (time1 - time0) / (plotSteps - 1);
+
+%r=ones(m,n).*r + 0.04*sqrt(vx.*vx+vy.*vy); % shrinkage correction
+r=ones(m,n).*r;  % make sure it is array
+
+data2=phi;
+data3=phi;
+tNow=time0;
+for i=1:plotSteps
+    tNext=min(time1, tNow + tPlot);
+%    % call the fortran implementation
+    [data3]=prop_test_f(data3,tNow,tNext,vx,vy,r,dx,dy);
+    %call own imp
+    %[ux,uy]=get_advection(phi,r,vx,vy,dx,dy);
+    %vis_wind(ux,uy,dx,dy)
+    if(i==9),keyboard,end
+    [tNow,data2]=prop_ls(data2,tNow,tNext,vx,vy,r,dx,dy,@spread_rate);
+    %[tNow,data2]=prop_ls(data2,tNow,tNext,ux,uy,0,dx,dy);
+    err_fort=norm(data3(:)-data2(:))
+    if err_fort>1e-10,
+        i,warning('large difference between matlab and fortran')
+    end
+    % display
+    vis(data2,vx,vy,dx,dy,tNow);
+end
+%err_fort=norm(data3(:)-data2(:))
+% call the toolbox routines 
+% for that need to be in toolboxls/Examples/OsherFedkiw
+if 0
+data = normal_advection(phi,time0,time1,vx,vy,r,dx,dy,'low');
+fprintf('\n');
+err_toolbox=norm(data(:)-data2(:))
+end 
+
+end
+
+%-------------------------------------------
+
+function vis(u,vx,vy,dx,dy,tNow)
+[m,n]=size(u);
+x=[0:m-1]*dx;
+y=[0:n-1]*dy;
+figure(1);clf
+hold off
+vis_type= '2d';
+drawn=false;
+switch vis_type
+    case '3d'
+    h=surf(y,x,u);
+    set(h,'edgecolor','none')
+    alpha(0.3)
+    hold on
+    contour3(y,x,u,[0 0],'k')
+    hold off
+    drawn=true;
+    case '2d'
+    [m,n]=size(u);
+    contour(y,x,u',[0 0],'k')
+    hold on
+    vis_wind(vx,vy,dx,dy)
+    axis equal
+    drawn=true;
+    otherwise
+        disp('no visualization selected, set vis=type to 2d or 3d')
+end
+if drawn,
+        title(sprintf('t=%g',tNow));
+    drawnow;
+    pause(0.3); % for ctrl-c
+end
+end
+
+function [phi_out]=prop_test_f(phi,ts,te,vx,vy,r,dx,dy)
+fmt='%25.15e \n';
+if any(any(isnan(phi))), error('phi NaN'), end
+[m,n]=size(phi);
+fid = fopen('prop_test_in.txt','wt');
+fprintf(fid,fmt,m,n,ts,te,r,dx,dy);
+fprintf(fid,fmt,phi,vx,vy);
+fclose(fid);
+! prop_test_prog.exe
+fid = fopen('prop_test_out.txt','rt');
+if fid < 0,
+    error('cannot open prop_test_out.txt')
+end
+[in,c]=fscanf(fid,'%g');
+fclose(fid);
+if c~=m*n,
+    fprintf('read %g terms should have %g\n',c,m*n+1)
+    error('bad number of terms in output file')
+end
+%t=in(1);
+phi_out=reshape(in(1:c),size(phi));
+if any(any(isnan(phi_out))), error('phi_out NaN'), end
+end
+
+function speed=spread_rate(r,vx,vy,nvx,nvy,scale);
+    speed = r + max(vx.*nvx + vy.*nvy,0);
+end
+
