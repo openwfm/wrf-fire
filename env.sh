ee4404dca50a324ec7c90fee540ef7192d9d9f09
Jonathan Beezley <jbeezley@opt4.cudenver.edu>
2007-10-23 09:19:16 -0600

    added support for opt4 environment

--- env.sh
+++ env.sh
@@ -1,7 +1,31 @@
-export NETCDF=/opt/wrf-libs/netcdf
-export JASPERLIB=/opt/wrf-libs/jasper/lib
-export JASPERINC=/opt/wrf-libs/jasper/include
-export NCARG=/opt/wrf-libs/ncarg
+
+cont=1
+host=`hostname`
+if [ ${host} = "wf.cudenver.edu" ] ; then
+  base="/opt/wrf-libs"
+elif [ ${host} = "opt4.cudenver.edu" ] ; then
+  base="/scratch0/jbeezley/wrf-libs"
+else
+  echo "unknown host: " ${host}
+  cont=0
+fi
+
+if [ $cont -eq 1 ] ; then
+export NETCDF=${base}/netcdf
+export JASPERLIB=${base}/jasper/lib
+export JASPERINC=${base}/jasper/include
+export NCARG=${base}/ncarg
 export NCARG_ROOT=$NCARG
-source /opt/intel/fce/9.1.036/bin/ifortvars.sh
 ulimit -s unlimited
+
+ifvars=/opt/intel/fce/9.1.036/bin/ifortvars.sh
+if [ -f ${ifvars} ] ; then
+  source ${ifvars}
+else
+  echo "WARNING: couldn't find ifort setup script"
+fi
+
+else
+
+echo "Quiting"
+fi
