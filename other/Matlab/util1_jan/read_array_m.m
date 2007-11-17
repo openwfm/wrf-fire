98068146a7a4369836321466e7e4c0fb161d643f
Jan Mandel <Jan.Mandel@wf.cudenver.edu>
2007-11-17 09:25:17 -0700

    support 3d arrays for matlab output

--- other/Matlab/util1_jan/read_array_m.m
+++ other/Matlab/util1_jan/read_array_m.m
@@ -3,10 +3,12 @@ b=load(f);
 l=length(b);
 m=b(1);
 n=b(2);
-s=m*n+2;
-fprintf(1,'matrix size %i %i from file %s length %i should be %i\n',m,n,f,l,s)
+o=b(3);
+s=m*n*o+3;
+fprintf(1,'matrix size %i %i %i from file %s length %i should be %i\n',...
+    m,n,o,f,l,s)
 if l~=s,
     error('bad format')
 end
-a=reshape(b(3:m*n+2),[m,n]);
+a=reshape(b(4:s),[m,n,o]);
 end
---
+++ other/Matlab/util1_jan/read_array_m2.m
@@ -0,0 +1,12 @@
+function a=read_array_m2(f);
+b=load(f);
+l=length(b);
+m=b(1);
+n=b(2);
+s=m*n+2;
+fprintf(1,'matrix size %i %i from file %s length %i should be %i\n',m,n,f,l,s)
+if l~=s,
+    error('bad format')
+end
+a=reshape(b(3:m*n+2),[m,n]);
+end
