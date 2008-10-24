3d08a569f726f84b5954586b2b6106cd54fd9b0d
Jan Mandel <jmandel@maccat.(none)>
2008-10-23 22:41:05 -0600

    read_array_tiles.m defaults to read_array_sp.m if tiles=[]

--- other/Matlab/util1_jan/read_array_tiles.m
+++ other/Matlab/util1_jan/read_array_tiles.m
@@ -3,6 +3,7 @@ function a=read_array_tiles(root,tiles,num1,num2);
 % read array produced by matching calls write_array_m
 % in module_fr_sfire_util.F
 % from files root[_num1[_num2]].nnnn.txt,  nnnn=1:ntiles
+% if tiles=[] same as read_array_sp(root,num1,num2)
 
 % Jan Mandel, 2008
 
@@ -13,6 +14,10 @@ if ~exist('num1','var'),
         num2=-1;
     end
     ff=file_name(root,num1,num2);
+end
+if isempty(tiles),
+    a=read_array_sp([ff,'.txt']);
+    return
 end
 for i=tiles
     f=sprintf('%s.%4.4i.txt',ff,i);
