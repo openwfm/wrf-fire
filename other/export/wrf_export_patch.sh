528f956ac3de248a250f204e25f4b561b576b314
Jonathan Beezley <jon.beezley.math@gmail.com>
2008-11-19 18:33:02 -0700

    changing fire permission flags to be compatible with svn

--- other/export/wrf_export_patch.sh
+++ other/export/wrf_export_patch.sh
@@ -20,17 +20,18 @@ EOF
 chmod +x wrfv2_fire/phys/commit_hash
 }
 
-if [ $(git diff HEAD | wc -l) -gt 0 ] ; then
-  echo "need to commit changes before exporting a patch"
-  exit 1
-fi
+#if [ $(git diff HEAD | wc -l) -gt 0 ] ; then
+#  echo "need to commit changes before exporting a patch"
+#  exit 1
+#fi
 
 replace_commit
 
 sv=$(git log -n 1 svntrack | grep -o 'r[0-9]\{4\}' |head -1)
 f=$(filter)
-git diff --stat svntrack $f
-git diff --relative=wrfv2_fire --diff-filter=ACDMR svntrack $f > wrf_svn_${sv}.patch
+git diff --stat --diff-filter=ACDMR svntrack $f
+git diff --relative=wrfv2_fire --diff-filter=ACDMR svntrack $f | gzip -c - > wrf_svn_${sv}.patch.gz
 
-git reset --hard HEAD > /dev/null
+#git reset --hard HEAD > /dev/null
+git checkout wrfv2_fire/phys/commit_hash
 
