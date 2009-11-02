1737e4ee22c78bcd1b1377ceeb12cddaec921ab1
Jan Mandel <Jan.Mandel@gmail.com>
2009-11-02 09:40:10 -0700

    showing K=1 and 2, hill semi-transparent to see the ground wind better

--- wrfv2_fire/test/em_fire/hill/hillframe.m
+++ wrfv2_fire/test/em_fire/hill/hillframe.m
@@ -1,5 +1,6 @@
 disp('1. install mexnc under the same directory as wrf-fire, then')
 disp('2. start matlab in wrf/WRFV3/test/em_fire to set up the paths properly')
+disp('   or cd there and run startup while in the directory')
 disp('3. read a wrfrst file into the workspace as in')
 disp('   >> ncload wrfrst_d01_0001-01-01_00:01:00')
 
@@ -24,9 +25,9 @@ end
 
 clf
 amin=[15,15,1];  % the atm grid part to show
-amax=[30,30,1];
+amax=[30,30,2];
 amin=[1,1,1];  % the atm grid part to show
-amax=[41,41,1];
+amax=[41,41,2];
 
 qstep=[5,5];        % quiver step for wind on the surface fire grid
 astep=[1,1,1];      % quiver step for wind on the atmosphere grid
@@ -47,7 +48,7 @@ hold on
 i=dmin(1):dmax(1);j=dmin(2):dmax(2);
 
 % surface colored by heat flux
-hs=surf(fxlong(i,j),fxlat(i,j),zsf(i,j),fgrnhfx(i,j),'EdgeColor','none'); colorbar
+hs=surf(fxlong(i,j),fxlat(i,j),zsf(i,j),fgrnhfx(i,j),'EdgeColor','none','FaceAlpha',0.7); colorbar
 [c,hc]=contour3(fxlong(i,j),fxlat(i,j),zsf(i,j));
 for h=hc(:)', set(h,'EdgeColor','black');end
 
