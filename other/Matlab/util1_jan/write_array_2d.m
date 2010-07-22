c45c17c810733e408a1635de1592069c7691da2c
Jan Mandel <Jan.Mandel@gmail.com>
2010-07-22 09:38:42 -0600

    comments

--- other/Matlab/util1_jan/write_array_2d.m
+++ other/Matlab/util1_jan/write_array_2d.m
@@ -1,4 +1,7 @@
 function write_array_2d(filename,a)
+% write_array_2d(filename,a)
+% write 2d matrix as input to wrf-fire
+% usage: write_array_2d('input_ht',ht)
 [m,n]=size(a);
 h=fopen(filename,'w');
 fprintf(h,'%6i\n',m,n);
