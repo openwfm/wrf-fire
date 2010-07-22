7507eb02e1dc97f86b55d2798252f13b9a36b7a5
Jan Mandel <Jan.Mandel@gmail.com>
2010-07-22 14:35:35 -0600

    correct htg in wrfinput_d01, correct image in matlab except vertical label

---
+++ other/Matlab/util1_jan/image_array_2d.m
@@ -0,0 +1,11 @@
+function image_array_2d(a)
+% write_array_2d(a)
+% Purpose: visualized 2d matrix written by write_array_2d
+% for input to WRF-Fire so that it looks the same as the array
+% in ncview from the arrays in WRF state
+%
+[m,n]=size(a);
+h=imagesc(flipud(a'));
+warning('the vertical label is upside down')
+colorbar
+end
--- other/Matlab/util1_jan/write_array_2d.m
+++ other/Matlab/util1_jan/write_array_2d.m
@@ -1,16 +1,19 @@
 function write_array_2d(filename,a)
 % write_array_2d(filename,a)
-% Purpose: write 2d matrix as input to wrf-fire
+% Purpose: write 2d matrix as input to WRF-Fire
 %
 % Arguments
 % filename  string, the name of the file
 % a         2d matrix, the array to be written
 %
 % Example: write_array_2d('input_ht',ht)
-% Note: you can read the file back by a=read_array_2d(filename)
+% 
+% See also: 
+%    read_array_2d  read the file back by a=read_array_2d(filename)
+%    image_array_2d visualize the array by image_array_2d(a)
 [m,n]=size(a);
 h=fopen(filename,'w');
 fprintf(h,'%6i\n',m,n);
-fprintf(h,'%14.7e\n',a);
+fprintf(h,'%14.7e\n',a');
 fclose(h);
 end
