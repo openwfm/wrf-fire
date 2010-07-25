cd2c54fdd92bc517219986a7e00c3df9cc46f289
Jan Mandel <Jan.Mandel@gmail.com>
2010-07-25 00:36:50 -0600

    added vprofile.m and netcdf aux, less verbose

--- other/Matlab/netcdf/ncgetgatt.m
+++ other/Matlab/netcdf/ncgetgatt.m
@@ -1,9 +1,9 @@
 function gattval=ncgetgatt(filename,gattname) 
 % get the value of global attribute
 quiet=exist('q','var');
-fprintf('ncdump: file %s\n',filename);
+% fprintf('ncgetatt: file %s\n',filename);
 ncid = netcdf.open(filename,'NC_NOWRITE');
 gattval = netcdf.getAtt(ncid,netcdf.getConstant('NC_GLOBAL'),gattname);
-disp(['attribute ',gattname,'=',num2str(gattval)])
+disp(['global attribute ',gattname,'=',num2str(gattval)])
 netcdf.close(ncid);
 end
