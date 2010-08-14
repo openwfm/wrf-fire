022389532f8dac6bcad7339772500d8f064bc72e
Jan Mandel <Jan.Mandel@gmail.com>
2010-08-13 22:38:19 -0600

    misc fixes to 3d matlab plots

--- other/Matlab/vis3d/plot_wind_at_h.m
+++ other/Matlab/vis3d/plot_wind_at_h.m
@@ -5,19 +5,27 @@ clf
 if ~exist('timestep','var'),
     timestep=1;
 end
+hgt=p.hgt(:,:,timestep); % terrain height
 % wind at given height
 for i=ilevels(:)'
     z=p.levels(i)*ones(size(x));
     ws=sqrt(p.uch(:,:,i,timestep).^2 + p.vch(:,:,i,timestep).^2);
-    surf(x,y,z,ws,'EdgeAlpha',alpha*0,'FaceAlpha',alpha)
+    print_layer_msg(i,z,ws)
+    surf(x,y,z+hgt,ws,'EdgeAlpha',alpha*0,'FaceAlpha',alpha)
     hold on, drawnow
 end
 for i=ilayers(:)'
-    z=p.altitude(:,:,i,timestep);
+    z=p.altitude(:,:,i,timestep)-hgt;
     ws=sqrt(p.uch(:,:,i,timestep).^2 + p.vch(:,:,i,timestep).^2);
-    surf(x,y,z,ws,'EdgeAlpha',alpha*0,'FaceAlpha',alpha)
+    print_layer_msg(i,z,ws)
+    surf(x,y,z+hgt,ws,'EdgeAlpha',alpha*0,'FaceAlpha',alpha)
     hold on, drawnow
 end
 colorbar
 hold off
+end
+
+function print_layer_msg(i,z,ws)
+    fprintf('level %i height %g to %g median %g horizontal wind speed %g to %g median %g\n',...
+       i,min(z(:)),max(z(:)),median(z(:)),min(ws(:)),max(ws(:)),median(ws(:)) )
 end
--- other/Matlab/vis3d/run_plot_wind_at_h.m
+++ other/Matlab/vis3d/run_plot_wind_at_h.m
@@ -1,9 +1,10 @@
 % run_plot_at_h
 % wrfout='fireflux_small/wrfout_d01_2006-02-23_12:43:00';
 timestep=5;
-levels=[1,2,6,50, 100, 250]; % in m
-ilayers=[1,3]; % indices
-ilevels=[3 4 5 6];  % indices
+levels=[1,2,6,46, 100, 250]; % in m, what gets computed
+ilevels=[4]; % indices, which levels from above get displayed
+ilevels=[];
+ilayers=[1]; % indices of mesh layers to display
 alpha=0.2;
 p=wrfatm2struct(wrfout,timestep)
 p=wind_uv_at_h(p,levels);
