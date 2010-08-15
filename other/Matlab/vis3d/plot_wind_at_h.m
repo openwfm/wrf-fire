8782d5fe09ddfe4f63ba85fbaeef6e64bf34acfc
Jan Mandel <Jan.Mandel@gmail.com>
2010-08-15 10:35:28 -0600

    Matlab/vis3d/run_plot_wind_at_h.m making an OK picture from fireflux_small

--- other/Matlab/vis3d/plot_wind_at_h.m
+++ other/Matlab/vis3d/plot_wind_at_h.m
@@ -1,28 +1,39 @@
-function plot_wind_at_h(p,ilevels,ilayers,alpha,timestep)
+function plot_wind_at_h(p,iheights,levels,alpha,timestep,windscale,heightscale)
 x=p.xlat;
 y=p.xlong;
 clf
 if ~exist('timestep','var'),
     timestep=1;
 end
+if ~isscalar(timestep),
+    error('timestep must be scalar')
+end
 hgt=p.hgt(:,:,timestep); % terrain height
 % wind at given height
-for i=ilevels(:)'
-    z=p.levels(i)*ones(size(x));
-    ws=sqrt(p.uch(:,:,i,timestep).^2 + p.vch(:,:,i,timestep).^2);
+for i=iheights(:)'
+    z=p.heights(i)*ones(size(x));
+    ws=sqrt(p.uch(:,:,i,timestep).^2 + p.vch(:,:,i,timestep).^2)
     print_layer_msg(i,z,ws)
     surf(x,y,z+hgt,ws,'EdgeAlpha',alpha*0,'FaceAlpha',alpha)
-    hold on, drawnow
+    hold on
 end
-for i=ilayers(:)'
+for i=levels(:)'
     z=p.altitude(:,:,i,timestep)-hgt;
-    ws=sqrt(p.uch(:,:,i,timestep).^2 + p.vch(:,:,i,timestep).^2);
+    ws=sqrt(p.uc(:,:,i,timestep).^2 + p.vc(:,:,i,timestep).^2)
     print_layer_msg(i,z,ws)
     surf(x,y,z+hgt,ws,'EdgeAlpha',alpha*0,'FaceAlpha',alpha)
-    hold on, drawnow
+    hold on
 end
+caxis([0,windscale])
+title(p.times,'Interpreter','none')
 colorbar
-hold off
+axis('image')
+a=[min(x(:)),max(x(:)),min(y(:)),max(y(:)),0,heightscale]; % var limits
+axis(a);     % set var limits
+xysize=max([a(4)-a(3),a(2)-a(1)]);
+zsize=a(6)-a(5);
+daspect([1,1,3*zsize/xysize]);
+hold off, drawnow
 end
 
 function print_layer_msg(i,z,ws)
--- other/Matlab/vis3d/wind_uv_at_h.m
+++ other/Matlab/vis3d/wind_uv_at_h.m
@@ -1,9 +1,9 @@
-function p=wind_uv_at_h(p,levels)
-% p=add2struct_wind_at_h(p,levels)
+function p=wind_uv_at_h(p,heights)
+% p=add2struct_wind_at_h(p,heights,levels)
 % read 
 % in: 
 % p           structure from wrfatm2struct
-% levels      1D array, heights above terrain to compute the wind
+% heights     1D array, heights above terrain to compute the wind
 % out: additional fields in p computed, value(:,:,i) is value at heights(i)
 % p.uch,p.vch         wind at cell centers (theta points, not staggered)
 % p.uah,p.vah         wind at u-points, v-points (staggered)
@@ -18,17 +18,17 @@ end
 % roughness of the ground under the u and v points
 [z0_bu,z0_bv]=interp_w2buv(p.z0);
 
-% log interpolation of the wind at u and v points to height levels
-p.uah=log_interp_vert(p.u,alt_bu,z0_bu,levels);
-p.vah=log_interp_vert(p.v,alt_bv,z0_bv,levels);
+% log interpolation of the wind at u and v points to height
+p.uah=log_interp_vert(p.u,alt_bu,z0_bu,heights);
+p.vah=log_interp_vert(p.v,alt_bv,z0_bv,heights);
 
 clear alt_bu alt_bv z0_bu z0_bv % free some memory
 
 % log interpolation of the wind at center points to height
-p.uch=log_interp_vert(p.uc,p.alt_at_w,p.z0,levels);
-p.vch=log_interp_vert(p.vc,p.alt_at_w,p.z0,levels);
+p.uch=log_interp_vert(p.uc,p.alt_at_w,p.z0,heights);
+p.vch=log_interp_vert(p.vc,p.alt_at_w,p.z0,heights);
 
-p.levels=levels;
+p.heights=heights;
 
 end
 
