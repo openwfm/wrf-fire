2613e0dbbf60c56b22fd4db1fb5c0ffe069f65ed
Jan Mandel <jmandel@frontend.cluster>
2010-08-28 21:01:07 -0600

    fixed parallel test scripts

--- wrfv2_fire/test/em_fire/test_hill.m
+++ wrfv2_fire/test/em_fire/test_hill.m
@@ -1,10 +1,12 @@
 % test if parallel runs give the same results
 runs={'1x1','1x6','6x1','2x3','2x3','1x2','2x1'}
 vars={'UAH','VAH','UF','VF'}
-p1=nc2struct(['hill-',runs{1},'/wrfrst_d01_0001-01-01_00:01:00'],vars,{},[])
-for i=2:length(cases)
+root='hill-';
+wrffile='wrfrst_d01_0001-01-01_00:01:00';
+p1=nc2struct([root,runs{1},'/',wrffile],vars,{},[])
+for i=2:length(vars)
     run=runs{i};
-    p=nc2struct(['hill-',run,'/wrfrst_d01_0001-01-01_00:01:00'],vars,{},[])
+    p=nc2struct([root,run,'/',wrffile],vars,{},[])
     run
     for j=1:length(vars)
         var=lower(vars{j})
