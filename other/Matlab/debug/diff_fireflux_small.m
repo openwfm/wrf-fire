9bede0a4cdeb1dec30510f42fd3292b7c4c9aa56
Jan Mandel <jmandel@frontend.cluster>
2010-08-29 22:26:11 -0600

    added check sums

--- other/Matlab/debug/diff_fireflux_small.m
+++ other/Matlab/debug/diff_fireflux_small.m
@@ -1,5 +1,6 @@
 % test if parallel runs give the same results
-runs={'6x1','1x12','3x4','4x3'}
+runs={'6x1','1x12.a','3x4','4x3'}
+runs={'1x1','6x1','1x12.a','1x12','3x4','4x3'}
 vars={'UAH','VAH','UF','VF'}
 root='fireflux_small-';
 wrffile='wrfrst_d01_2006-02-23_12:47:00';
