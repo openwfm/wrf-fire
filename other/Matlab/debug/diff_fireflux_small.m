a1b618b20bd537b3e3e3c860633c76d28d425420
Jan Mandel <jmandel@frontend.cluster>
2010-08-31 14:04:50 -0600

    index fixes and debug changes

--- other/Matlab/debug/diff_fireflux_small.m
+++ other/Matlab/debug/diff_fireflux_small.m
@@ -1,7 +1,13 @@
+function diff_fireflux_small(timestep)
 % test if parallel runs give the same results
-runs={'6x1','1x12.a','3x4','4x3'}
-runs={'1x1','6x1','1x12.a','1x12','3x4','4x3'}
+runs={'12x1','6x1','1x12','3x4','3x4a','4x3'}
+runs={'1x1','6x1','1x12.a','1x12','3x4','3x4a','4x3'}
+runs={'1x1a','1x12a'}
+
 vars={'UAH','VAH','UF','VF'}
 root='fireflux_small-';
 wrffile='wrfrst_d01_2006-02-23_12:47:00';
-diff_runs(runs,vars,root,wrffile) 
+wrffile='wrfout_d01_2006-02-23_12:42:00';
+% timestep=10
+diff_runs(runs,vars,root,wrffile,timestep) ;
+end
--- other/Matlab/debug/diff_runs.m
+++ other/Matlab/debug/diff_runs.m
@@ -1,16 +1,18 @@
-function err=diff_runs(runs,vars,root,wrffile)
+function err=diff_runs(runs,vars,root,wrffile,timestep)
 % test if runs give the same results
 % example
 %runs={'1x1','1x6','6x1','2x3','2x3','1x2','2x1'}
 %vars={'UAH','VAH','UF','VF'}
 %root='hill-';
 %wrffile='wrfrst_d01_0001-01-01_00:01:00';
-%diff_runs(runs,vars,root,wrffile)
+%diff_runs(runs,vars,root,wrffile,[])
+%wrffile='wrfout_d01_2006-02-23_12:42:00'
+%diff_runs(runs,vars,root,wrffile,1)
 
-p1=nc2struct([root,runs{1},'/',wrffile],vars,{},[])
+p1=nc2struct([root,runs{1},'/',wrffile],vars,{},timestep)
 for i=2:length(runs)
     run=runs{i};
-    p=nc2struct([root,run,'/',wrffile],vars,{},[])
+    p=nc2struct([root,run,'/',wrffile],vars,{},timestep)
     run
     for j=1:length(vars)
         var=lower(vars{j})
@@ -18,3 +20,6 @@ for i=2:length(runs)
         err(i,j)=e;
     end
 end
+timestep
+runs
+err
