d46d3ebad839f229f2ffc925955ee7d112fd61a8
Jan Mandel <jmandel@frontend.cluster>
2010-08-31 21:24:35 -0600

    more changes in matlab scripts

--- other/Matlab/debug/diff_fireflux_small.m
+++ other/Matlab/debug/diff_fireflux_small.m
@@ -2,7 +2,7 @@ function diff_fireflux_small(timestep)
 % test if parallel runs give the same results
 % runs={'12x1','6x1','1x12','3x4','3x4a','4x3'}
 % runs={'1x1','6x1','1x12.a','1x12','3x4','3x4a','4x3'}
-runs={'1x1a','1x12a','3x4a','12x1','4x3','3x4','1x12'}
+runs={'1x1a','1x12a','3x4a','12x1','4x3','1x12'}
 vars={'UAH','VAH','UF','VF'}
 root='fireflux_small-';
 wrffile='wrfrst_d01_2006-02-23_12:47:00';
