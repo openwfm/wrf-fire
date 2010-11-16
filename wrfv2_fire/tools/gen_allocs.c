3e6f55b8b26240affb811a9a4c1bf6d52ee491bb
Jonathan Beezley <jon.beezley@gmail.com>
2010-11-16 09:21:12 -0700

    reverting bug fix to registry program in favor of John M.'s fix

--- wrfv2_fire/tools/gen_allocs.c
+++ wrfv2_fire/tools/gen_allocs.c
@@ -180,7 +180,7 @@ if ( tag == 1 )
             fprintf(fp, "IF ( setinitval .EQ. 3 ) %s%s=initial_data_value\n",
                         structname ,
                         fname ) ;
-          } else if ( !strcmp( p->type->name , "integer" ) && strcmp(fname,"sr_x") && strcmp(fname,"sr_y") ) {
+          } else if ( !strcmp( p->type->name , "integer" ) ) {
             fprintf(fp, "IF ( setinitval .EQ. 3 ) %s%s=0\n",
                         structname ,
                         fname ) ;
