ed8cd5df1472013024928becc695de0a9047b960
Jonathan Beezley <jon.beezley.math@gmail.com>
2010-11-16 18:44:05 -0700

    fixing create_links.sh

--- wrfv2_fire/test/em_fire/create_links.sh
+++ wrfv2_fire/test/em_fire/create_links.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for f in * ; do
-  if [ -d f ] ; then
+  if [ -d $f ] ; then
     for x in "fire.exe" "wrf.exe" "ideal.exe" "fire_input.nc" ; do
       ln -sf ../${x} ${f}/${x}
     done
