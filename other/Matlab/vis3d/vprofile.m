88be51c6ad74e4123a0ed55c72bbb48e1976f246
Jan Mandel <jmandel@frontend.cluster>
2011-07-30 20:45:29 -0600

    more prints on terminal in vprofile

--- other/Matlab/vis3d/vprofile.m
+++ other/Matlab/vis3d/vprofile.m
@@ -1,8 +1,9 @@
-function vprofile(filename,x1,x2,tstep)
-% vprofile(filename,loc,tstep)
+function vprofile(p,x1,x2,tstep)
+% vprofile(filename,x1,x2,tstep)
 % compute and plot vertical profiles at given location and time
 % input
-% filename      string, WRF file name
+% p             string, WRF file name, or structure p where the file was read using the command
+%               p=wrfatm2struct(filename,tstep);       
 % x1            distance in m from lower left end of domain in coordinate 1
 % x2            distance in m from lower left end of domain in coordinate 2
 % tstep         the index in input files to compute this at, or empty
@@ -12,10 +13,14 @@ function vprofile(filename,x1,x2,tstep)
 %             vprofile('wrfout_d01_0001-01-01_00:00:00',100,100,1)
 
 % the data for this time step
-p=wrfatm2struct(filename,tstep);  
+if ~isstruct(p),
+    p=wrfatm2struct(filename,tstep);  
+end
 % horizontal coordinates in terms of mesh index, cell centered 
 i1=x1/p.dx-0.5;
 i2=x2/p.dy-0.5;
+fi1=x1/p.fdx-0.5;
+fi2=x2/p.fdy-0.5;
 % interpolate the vertical profiles
 altitude=interp_12(p.altitude,i1,i2);
 
@@ -28,13 +33,36 @@ v=interp_12(p.v,i1,i2+0.5);  % staggered in x2
 direction=180*direction/pi;  % convert to degrees
 
 layers=5;
-fprintf('Wind profile at %gm %gm from lower left corner of domain\n',x1,x2)
-fprintf('Time step %i at %s\n',tstep,p.times{1})
+fprintf('Wind profile at (%gm %gm) from lower left corner of domain\n',x1,x2)
 z0 = interp2(p.z0, i1, i2);
-fprintf('Rougness height %5.3fm\n',z0);
-fprintf('layer altitude     U      V     speed\n')  
+dz0=z0;  % for display
+fprintf('Rougness height from LANDUSE %5.3fm\n',z0);
+fz0 = interp2(p.fz0, fi1, fi2);
+if fz0 > 0,
+fprintf('Roughness height on fire mesh %5.3fm\n',fz0);
+  dz0=fz0;
+end
+fwh = interp2(p.fwh, fi1, fi2);
+if fwh > 0,
+  fprintf('Wind height fire mesh        %5.3fm\n',fwh);
+end
+uf = interp2(p.uf, fi1, fi2);
+vf = interp2(p.vf, fi1, fi2);
+[df,sf]=cart2pol(-vf,uf);
+if sf<=0,
+  fprintf('No fuel at this location, wind not interpolated.\n') 
+end
+
+fprintf('layer altitude     U       V   speed  direction (degrees)\n')  
+fprintf('z0   %7.3f %7.3f %7.3f %7.3f\n',dz0,0,0,0)  % at roughness height
+
+if fwh>0,
+     fprintf('fwh  %7.3f %7.3f %7.3f %7.3f %7.3f\n',fwh,uf,vf,sf,df)
+elseif sf>0,
+     fprintf('             %7.3f %7.3f %7.3f %7.3f\n',uf,vf,sf,df)
+end
 for i=1:layers,
-     fprintf('%3i  %7.3f %7.3f %7.3f %7.3f \n',i,altitude(i),u(i),v(i),speed(i))
+     fprintf('%3i  %7.3f %7.3f %7.3f %7.3f %7.3f\n',i,altitude(i),u(i),v(i),speed(i),direction(i))
 end
 
 
--- other/Matlab/vis3d/wrfatm2struct.m
+++ other/Matlab/vis3d/wrfatm2struct.m
@@ -14,7 +14,8 @@ function p=wrfatm2struct(filename,timesteps)
 if ~exist('timesteps','var'),
     timesteps=[];
 end
-p=nc2struct(filename,{'U','V','W','PH','PHB','HGT','QVAPOR','T','Z0',...
+p=nc2struct(filename,{'U','V','W','PH','PHB','HGT','Z0','T',...
+    'FZ0','FWH','UF','VF',...
     'XLONG','XLAT','GRNHFX','FGRNHFX','FXLONG','FXLAT','Times'},...
     {'DX','DY'},timesteps);
 
@@ -26,6 +27,13 @@ for k=1:size(p.altitude,3)
     p.height(:,:,k,:)=p.altitude(:,:,k,:)-p.alt_at_w(:,:,1,:);
 end
 
+% refinement ratios
+p.sr_x=size(p.fxlong,1)/size(p.xlong,1);
+p.sr_y=size(p.fxlong,2)/size(p.xlong,2);
+
+% fire mesh step
+p.fdx = p.dx/p.sr_x;
+p.fdy = p.dy/p.sr_y;
 
 % add wind at centers (theta points)
 p.uc = 0.5*(p.u(1:end-1,:,:,:) + p.u(2:end,:,:,:));
