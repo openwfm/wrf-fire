dcf8214085d74bdf798c9aadc73a67c4ea9b67b3
Jan Mandel <jmandel@frontend.cluster>
2011-11-23 19:40:14 -0700

    generalizing isolated.m

--- other/Matlab/util1_jan/isolated.m
+++ other/Matlab/util1_jan/isolated.m
@@ -1,10 +1,21 @@
-function i=isolated(l,scheme)
-if ~exist('scheme','var'),
-  scheme=5;
-end
-i=l(2:end-1,2:end-1,:)<=0 & ...
+function i=isolated(l,d)
+% return the location of all isolated elements where l<0
+if ~exist('d','var'),
+  disp('default 5 point scheme') 
+  i=l(2:end-1,2:end-1,:)<=0 & ...
   l(1:end-2,2:end-1,:)>0 & l(3:end,2:end-1,:)>0 & l(2:end-1,1:end-2,:) >0 & l(2:end-1,3:end,:)>0 ;
-if scheme>5,
-i=i & l(1:end-2,1:end-2,:)>0 & l(1:end-2,3:end,:) > 0 & l(3:end,1:end-2,:)>0 & l(3:end,3:end,:) >0;
+else
+  fprintf('looking within distance %g ...',d)
+  i=l(1+d:end-d,1+d:end-d,:) <= 0;
+  for ii=-d:d,
+    for jj=-d:d,
+       if ii ~= 0 | jj ~= 0,
+          fprintf(' %i %i',ii,jj)
+          i=i & l(1+d+ii:end-d+ii,1+d+jj:end-d+jj,:) > 0;
+       end
+    end
+  end
+  fprintf('\n')
+end
 end
 
