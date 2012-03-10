b55fba3f8ce9983ef54005c7ed871c69135aba9b
Jan Mandel <Jan.Mandel@gmail.com>
2012-03-10 13:44:00 -0700

    improving standalone rain test case

--- standalone/make_rain_input.m
+++ standalone/make_rain_input.m
@@ -1,5 +1,17 @@
-function make_rain_input(n)
-% create moisture_input.txt for a given number of time steps
-% should give 1.723 fotr 14-hour wetting time lag
-mm=[14*[0:n]'/n,ones(n+1,1)*[300,1e5,0.01],1e3*[0:n]'/n];
+function make_rain_input(n,mmph,hours)
+% create moisture_input.txt for moisture_test.exe 
+% usage: make_rain_input(n,mmph)
+% input: 
+%    n        number of time steps
+%    mmph     rain intensity mm/h
+%    hours    length of simulation in hours
+% example
+% make_rain_input(n,100,14) should give 1.723 for 14-hour wetting time lag
+%
+% note: the last line in the output from moisture_test.exe
+%       should not depend on n
+
+mm=[hours*[0:n]'/n,ones(n+1,1)*[300,1e5,0.01],mmph*hours*[0:n]'/n];
 save('moisture_input.txt','mm','-ascii')
+
+end
