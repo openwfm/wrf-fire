6b6e0da3515980ba554f70eaecea8c54948d6f73
Jan Mandel <Jan.Mandel@gmail.com>
2012-06-06 07:54:39 +0200

    fixing up moisture figure for the wrf2012 paper

--- standalone/run_moisture_test.m
+++ standalone/run_moisture_test.m
@@ -19,8 +19,10 @@ setenv('DYLD_LIBRARY_PATH','/opt/local/lib:/opt/local/lib/gcc46')
 system('./moisture_test.exe');
 load moisture_output.txt
 out.t2   =moisture_output(:,3);
+out.t2_mid=midpoints(out.t2);
 out.psfc =moisture_output(:,4);
 out.q2   =moisture_output(:,5);
+% rh_fire is computed from midpoint values in the fortran code
 out.rain=moisture_output(:,6);
 out.rh_fire=moisture_output(:,7);
 out.fmc_equi =moisture_output(:,8);
@@ -29,8 +31,10 @@ out.fmc_gc   =moisture_output(:,10);
 out.id='from moisture_test.exe';
 out.id='';
 out.hours=hours;
+out.hours_mid=midpoints(hours);
 out.r=find(hours>=hr(1) & hours<=hr(2)); % range to display
 orig.t2=t2(~d);
+orig.t2_mid=midpoints(orig.t2);
 orig.psfc=psfc(~d);
 orig.q2=q2(~d);
 orig.rain=rain(~d);
@@ -39,6 +43,7 @@ orig.fmc_equi=fmc_equi(1,~d)';
 orig.fmc_gc=fmc_gc(1,~d)';
 orig.id=['from ',ncfile];
 orig.hours=hours;
+orig.hours_mid=midpoints(hours);
 orig.r=out.r;
 figure(1)
 plot(hours,orig.fmc_gc,'r--',hours,out.fmc_gc,'k-')
@@ -62,30 +67,30 @@ end
 
 function plot_moisture(f,s)
 figure(f);
-[d,w]=equilibrium_moisture(s.rh_fire,s.t2);
-plot(s.hours(s.r),s.fmc_equi(s.r),'r--',...
-     s.hours(s.r),d(s.r),'b--',...
-     s.hours(s.r),w(s.r),'b-.',...
+[d,w]=equilibrium_moisture(s.rh_fire,s.t2_mid);
+plot(s.hours_mid(s.r),d(s.r),'b--',...
+     s.hours_mid(s.r),w(s.r),'b-.',...
+     s.hours_mid(s.r),s.fmc_equi(s.r),'r--',...
      s.hours(s.r),s.fmc_gc(s.r),'k-')
 xlabel hours
 set(ylabel('Fuel moisture contents (kg / kg)'),'Interpreter','none')
 h=title(['Fuel moisture ',s.id]);
 set(h,'Interpreter','none')
-h=legend('Equilibrium','Drying','Wetting','Actual');
+h=legend('Drying','Wetting','Equilibrium','Actual');
 set(h,'Interpreter','none')
 setvmax(0.3)
 end
 
 function plot_all(f,s)
 figure(f)
-[d,w]=equilibrium_moisture(s.rh_fire,s.t2);
+[d,w]=equilibrium_moisture(s.rh_fire,s.t2_mid);
 plot(s.hours(s.r),s.t2(s.r)/max(s.t2),...
     s.hours(s.r),s.psfc(s.r)/max(s.psfc),...
     s.hours(s.r),s.q2(s.r)/max(s.q2),...
     s.hours(s.r),s.rh_fire(s.r),...
-    s.hours(s.r),s.fmc_equi(s.r),...
-    s.hours(s.r),d(s.r),'g-.',...
-    s.hours(s.r),w(s.r),'b-.',...
+    s.hours_mid(s.r),s.fmc_equi(s.r),...
+    s.hours_mid(s.r),d(s.r),'g-.',...
+    s.hours_mid(s.r),w(s.r),'b-.',...
     s.hours(s.r),s.fmc_gc(s.r))
 h=title(['All variables ',s.id]);
 set(h,'Interpreter','none');
@@ -106,4 +111,8 @@ function setvmax(v)
 a=axis;
 a(3:4)=[0,v];
 axis(a)
+end
+
+function T=midpoints(T2)
+T=[T2(1);0.5*(T2(1:end-1)+T2(2:end))];
 end
