e271734a61278396d34da96636818b30c6674d22
Volodymyr Kondratenko <vkondrat@Volodymyr-Kondratenkos-MacBook-Pro.local>
2012-11-23 02:57:29 -0700

    Added printing of the final tigh

--- other/Matlab/perimeter/main_function.m
+++ other/Matlab/perimeter/main_function.m
@@ -9,6 +9,12 @@ addpath('../../other/Matlab/netcdf');
 
 [long,lat,time_now,bound]=read_file_perimeter(data,'wrfout_d01_2007-10-21_12:00:00_real_case');
 V=ones(2,size(long,1),size(long,2));
-result=perimeter(long,lat,time_now,bound,V);
+result= to the file data_out.txt
+fid = fopen('data_out_tign.txt', 'w');
+dlmwrite('data_out_tign.txt', tign, 'delimiter', '\t','precision', '%.4f');
+fclose(fid);
+'printed'
+
+write_array_2d('data_out_wrf_tign.txt',B)perimeter(long,lat,time_now,bound,V);
 
 
