4a1489ff155944e6f2dd15f6852fc99add397f50
Volodymyr Kondratenko <vkondrat@Volodymyr-Kondratenkos-MacBook-Pro.local>
2012-11-23 02:53:10 -0700

    added addpath to netcdf folders

--- other/Matlab/perimeter/main_function.m
+++ other/Matlab/perimeter/main_function.m
@@ -3,6 +3,9 @@ data='data_perim.txt';
 %t=perimeter(data,V)
 
 addpath ../../other/Matlab/perimeter
+addpath('../../other/Matlab/util1_jan');
+addpath('../../other/Matlab/netcdf');
+
 
 [long,lat,time_now,bound]=read_file_perimeter(data,'wrfout_d01_2007-10-21_12:00:00_real_case');
 V=ones(2,size(long,1),size(long,2));
