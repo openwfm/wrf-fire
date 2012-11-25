a3b1762d35750faa233ac3a81cf0813c34ce922b
Volodymyr Kondratenko <vkondrat@Volodymyr-Kondratenkos-MacBook-Pro.local>
2012-11-25 11:51:42 -0700

    Updated the main_function

--- other/Matlab/perimeter/main_function.m
+++ other/Matlab/perimeter/main_function.m
@@ -6,15 +6,14 @@ addpath ../../other/Matlab/perimeter
 addpath('../../other/Matlab/util1_jan');
 addpath('../../other/Matlab/netcdf');
 
-
 [long,lat,time_now,bound]=read_file_perimeter(data,'wrfout_d01_2007-10-21_12:00:00_real_case');
 V=ones(2,size(long,1),size(long,2));
-result= to the file data_out.txt
+tign=perimeter(long,lat,time_now,bound,V);
+
+% Writing the data to the file data_out.txt
 fid = fopen('data_out_tign.txt', 'w');
 dlmwrite('data_out_tign.txt', tign, 'delimiter', '\t','precision', '%.4f');
 fclose(fid);
 'printed'
 
-write_array_2d('data_out_wrf_tign.txt',B)perimeter(long,lat,time_now,bound,V);
-
-
+write_array_2d('data_out_wrf_tign.txt',B)
