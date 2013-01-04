f4ef0067a1d9c245e22526f099b0c73fda879f94
Volodymyr Kondratenko <vkondrat@login02.rc.colorado.edu>
2013-01-04 16:43:32 -0700

    Changed data input and main_function.m specifically for Adams witch&Guejito problrm

--- other/Matlab/perimeter/main_function.m
+++ other/Matlab/perimeter/main_function.m
@@ -1,11 +1,13 @@
-data='data_perim.txt';
+data='data_perimeter.txt';
+wrfout='wrfout_d04_2007-10-21_12_00_00';
 
 addpath ../../other/Matlab/perimeter
 addpath('../../other/Matlab/util1_jan');
 addpath('../../other/Matlab/netcdf');
 addpath('../../other/Matlab/vis3d');
+addpath /home/vkondrat/work/wrfout_files
 
-[long,lat,uf,vf,dzdxf,dzdyf,time_now,bound]=read_file_perimeter(data,'wrfout_d01_2007-10-21_12:00:00_real_case');
+[long,lat,uf,vf,dzdxf,dzdyf,time_now,bound]=read_file_perimeter(data,wrfout,49);
 tign=perimeter(long,lat,uf,vf,dzdxf,dzdyf,time_now,bound);
 
 % Writing the data to the file data_out.txt
