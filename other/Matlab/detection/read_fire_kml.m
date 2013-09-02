4b4e76cc034f877197b5ce42ac7e7559f692341b
Jan Mandel <Jan.Mandel@gmail.com>
2013-09-01 22:45:34 -0600

    adding output dims to read_fire_kml

--- other/Matlab/detection/read_fire_kml.m
+++ other/Matlab/detection/read_fire_kml.m
@@ -8,7 +8,9 @@ function v=read_fire_kml(filename)
 %         v.lon       longitude
 %         v.lat       latitude
 %         v.tim       detection time (days, use datestr to convert to a string)
+%         v.res       resolution
 
+disp(['reading fire KML file ',filename])
 fid=fopen(filename);
 if fid < 0
     error(['could not find file ',filename])
@@ -18,10 +20,11 @@ s=10000;
 v.lat=zeros(s,1);
 v.lon=zeros(s,1);
 v.tim=zeros(s,1);
+v.res=zeros(s,1);
 while 1
     fline = fgetl(fid);
     if ~ischar(fline),
-        fclose(fid)
+        fclose(fid);
         break
     end % end of file
     f=strfind(fline,'Fire Detection Centroid ');
@@ -35,9 +38,18 @@ while 1
         flon=parse(dline,'<b>Longitude: </b>','<br/>');
         fdate=parse(dline,'<b>Detection Date: </b>','<br/>');
         ftime=parse(dline,'<b>Detection Time: </b>','<br/>');
+        sensor=parse(dline,'<b>Sensor: </b>','<br/>');
         v.lat(k)=str2num(flat);
         v.lon(k)=str2num(flon);
-        v.tim(k)=datenum([fdate,' ',ftime]);
+        v.tim(k)=datenum([fdate,' ',ftime],'dd mmm yyyy HH:MM');
+        switch sensor
+            case 'NPP VIIRS'
+                v.res(k)= 750;
+            case {'Terra MODIS','Aqua MODIS'}
+                v.res(k)= 1000;
+            otherwise
+                error(['unknown sensor ',sensor])
+        end
     end
 end
 v.lat=v.lat(1:k);
