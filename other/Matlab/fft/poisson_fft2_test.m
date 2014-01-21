77e8fc735d3096705e3a8b140607f309f0fa82c0
Jan Mandel <Jan.Mandel@gmail.com>
2014-01-21 16:21:59 -0700

    adding power of laplacian

--- other/Matlab/fft/poisson_fft2_test.m
+++ other/Matlab/fft/poisson_fft2_test.m
@@ -4,10 +4,15 @@ n=[111,155]; % gridpoints in each direction excluding boundaries
 h=rand(1,2); % meshstep
 
 F=ones(n);
+F=rand(n);
 
-U=poisson_fft2(F,h);
+U=poisson_fft2(F,h,1);
 FF=mlap(U,h);
 R=F-FF;
-err_PS=norm(R,inf)
+err=norm(R,inf)
+F2=poisson_fft2(U,h,-1/2);
+F2=poisson_fft2(F2,h,-1/2);
+R=F-F2;
+err=norm(R,inf)
 
 return
