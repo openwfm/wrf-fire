b332a2bf006a5d7a71bc44ac50400be6e0978b68
Volodymyr Kondratenko <vkondrat@Volodymyr-Kondratenkos-MacBook-Pro.local>
2014-02-16 10:14:10 -0700

    Now works for multiple wrfouts

--- other/Matlab/perimeter/main_function.m
+++ other/Matlab/perimeter/main_function.m
@@ -3,16 +3,16 @@ clc
 addpath ../netcdf
 addpath ../util1_jan
 
-wrfout{1}='wrfout_d01_0001-01-01_00:00:00_smallhill';
-wrfout{2}='';
-wrfout{3}='';
-wrfout{4}='';
+wrfout{1}='wrfout_d01_0001-01-01_00:00:00';
+wrfout{2}='wrfout_d01_0001-01-01_00:01:00';
+wrfout{3}='wrfout_d01_0001-01-01_00:02:00';
+wrfout{4}='wrfout_d01_0001-01-01_00:03:00';
 wrfout{5}='';
 
-num_wrf=1;  %The total number of wrfouts that are being used
+num_wrf=4;  %The total number of wrfouts that are being used
 time_step=6;     % The amount of time_steps in each wrfout
-interval=60;      % time step in wrfout in seconds (every 1 min)
-time=6;           % the number of the time step in the latest wrfout 
+interval=10;      % time step in wrfout in seconds (every 1 min)
+time=1;           % the number of the time step in the latest wrfout 
                   %  closest to the perimeter time from below
 format long
 
