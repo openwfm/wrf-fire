628a45caa016a7f7ae3e35a673217babca07c795
Jan Mandel <jmandel@macssd>
2016-02-22 06:00:01 +0000

    splitting off the the file load and visualization

---
+++ other/Matlab/detection/readmod14files.m
@@ -0,0 +1,39 @@
+function r=readmod14files(file_search,ax)
+
+% find the files
+d=dir(file_search);d={d.name};
+if(isempty(d)), error(['No files found for ',file_search]),end
+prefix=regexp(file_search,'^.*/','match');  % all up to the last /
+if isempty(prefix),  % comes as cell, convert to string
+    prefix='';
+else
+    prefix=prefix{1};
+end
+
+% order the files in time
+nfiles=length(d);
+t=zeros(1,nfiles);
+for i=1:nfiles
+    t(i)=rsac2time(d{i});
+end
+[t,i]=sort(t);
+d={d{i}};
+
+k=0;
+for i=1:nfiles,
+    file=d{i};
+    v=readmod14([prefix,file]);
+     % select fire detection within the domain
+    xj=find(v.lon >= ax(1) & v.lon <= ax(2));
+    xi=find(v.lat >= ax(3) & v.lat <= ax(4));
+    if ~isempty(xi) & ~isempty(xj)
+        k=k+1;
+        v.data=v.data(xi,xj);    % subset data
+        v.lon=v.lon(xj);
+        v.lat=v.lat(xi);
+        r.time(k)=t(i);
+        v.axis=ax;
+        r.x{k}=v;
+    end
+end
+end
