eec7fc58a96226fba79fccf43cba433fb36afe3d
Jan Mandel <jmandel@macssd>
2016-04-07 22:58:12 -0600

    spin off subset_domain.m from detect_fit_level2.m

--- other/Matlab/detection/detect_fit_level2.m
+++ other/Matlab/detection/detect_fit_level2.m
@@ -55,51 +55,7 @@ fuel=[]; fuels
 
 disp('Subset space from simulation')
 
-sim.min_lat = min(w.fxlat(:));
-sim.max_lat = max(w.fxlat(:));
-sim.min_lon = min(w.fxlong(:));
-sim.max_lon = max(w.fxlong(:));
-sim.min_tign= min(w.tign_g(:));
-sim.max_tign= max(w.tign_g(:));
-act.x=find(w.tign_g(:)<sim.max_tign);
-act.min_lat = min(w.fxlat(act.x));
-act.max_lat = max(w.fxlat(act.x));
-act.min_lon = min(w.fxlong(act.x));
-act.max_lon = max(w.fxlong(act.x));
-margin=0.5;
-fprintf('enter relative margin around the fire (%g)',margin);
-in=input(' > ');
-if ~isempty(in),margin=in;end
-min_lon=max(sim.min_lon,act.min_lon-margin*(act.max_lon-act.min_lon));
-min_lat=max(sim.min_lat,act.min_lat-margin*(act.max_lat-act.min_lat));
-max_lon=min(sim.max_lon,act.max_lon+margin*(act.max_lon-act.min_lon));
-max_lat=min(sim.max_lat,act.max_lat+margin*(act.max_lat-act.min_lat));
-
-default_bounds{1}=[min_lon,max_lon,min_lat,max_lat];
-default_bounds{2}=[sim.min_lon,sim.max_lon,sim.min_lat,sim.max_lat];
-for i=1:length(default_bounds),fprintf('default bounds %i: %8.5f %8.5f %8.5f %8.5f\n',i,default_bounds{i});end
-
-bounds=input('enter bounds [min_lon,max_lon,min_lat,max_lat] or number of bounds above (1)> ');
-if isempty(bounds),bounds=1;end
-if length(bounds)==1,
-    bounds=default_bounds{bounds};
-end
-[ii,jj]=find(w.fxlong>=bounds(1) & w.fxlong<=bounds(2) & w.fxlat >=bounds(3) & w.fxlat <=bounds(4));
-ispan=min(ii):max(ii);
-jspan=min(jj):max(jj);
-if isempty(ispan) | isempty(jspan), error('selection empty'),end
-
-% restrict simulation
-
-red.fxlat=w.fxlat(ispan,jspan);
-red.fxlong=w.fxlong(ispan,jspan);
-red.tign_g=w.tign_g(ispan,jspan);
-red.nfuel_cat=w.nfuel_cat(ispan,jspan);
-
-red.min_lat = min(red.fxlat(:));
-red.max_lat = max(red.fxlat(:));
-red.min_lon = min(red.fxlong(:));
-red.max_lon = max(red.fxlong(:));
+red=subset_domain(w);
 
 % convert tign_g to datenum 
 w.time=datenum(char(w.times)');
@@ -324,7 +280,7 @@ p.red.tign_g = red.max_tign_g + (24*60*60)*(analysis - w.time);
 
 % analysis = max_sim_time + (24*60*60)*(tign+h
 p.tign_g=w.tign_g;
-p.tign_g(ispan,jspan)=p.red.tign_g;
+p.tign_g(red.ispan,red.jspan)=p.red.tign_g;
 
 % max_sim_time + (24*60*60)*(tign+h + base_time - w_time_datenum);
 
