705b763c2285311c5bae377026424bfa343bc8f2
Jan Mandel <jmandel@macssd>
2016-05-17 22:37:15 -0600

    reverting time in propagate.m

--- other/Matlab/perimeter_new/propagate.m
+++ other/Matlab/perimeter_new/propagate.m
@@ -1,11 +1,12 @@
-function tign=propagate(tign,dir,fire_area,distance,ros,time_now)
+function tign=propagate(tign,dir,fire_area,distance,ros,time_now,print)
 % input:
 %   tign - ignition time, matrix size (m,n).
 %          Points not burning should be > t_now (dir=1) or < t_now (dir=-1)
 %   dir  - 1 forward, -1 backward
 %   fire_area - 1 what can burn, 0 what not
 %   distance(m,n,3,3) - distance
-%   t_now - the latest time to propagate to (earliest if dir=-1)
+%   time_now - the latest time to propagate to (earliest if dir=-1)
+%   print  1 for tracing
 % output:
 %   tign - fire arrival time up to t_now
 
@@ -21,35 +22,38 @@ function tign=propagate(tign,dir,fire_area,distance,ros,time_now)
 %   t(i,j,:,:)=fire arrival time already burning
 %   
 
+if abs(dir) ~= 1, error('dir must be +-1'),end 
 m=size(tign,1);n=size(tign,2);
 t=zeros(m,n,3,3);
 for i=1:m, for j=1:n
         t(i,j,:,:)=tign(i,j);
 end,end
-print=1;
+if ~exist('print','var'),
+    print=0;
+end
 d=distance;
-active=squeeze(t(:,:,2,2)<time_now);
+active=squeeze(dir*(time_now-t(:,:,2,2))>0);
 for step=1:100,
     t_old=t;
     for i=1:m, for j=1:n,
         if active(i,j) & fire_area(i,j)
             for a=1:3, for b=1:3, if a ~=2 | b~=2,
-                if print,fprintf('step %i point %i %i direction %i %i time %g ',step,i,j,a-2,b-2,t(i,j,a,b));end
-                dt = max(time_now-t(i,j,a,b),0);    % time available to propagate
+                if print>1,fprintf('step %i point %i %i direction %i %i time %g ',step,i,j,a-2,b-2,t(i,j,a,b));end
+                dt = max(dir*(time_now-t(i,j,a,b)),0);    % time available to propagate
                 if dt>0,
                     dd = dt.*ros(i,j,a,b);        % distance traveled to tnow
                     if d(i,j,a,b)> dd,              % positive distance remains
                         t(i,j,a,b)=time_now;
                         d(i,j,a,b)= d(i,j,a,b)-dd;  % decrease the distances remaining
-                        if print,fprintf('distance remaining %g time %g',d(i,j,a,b),time_now);end
+                        if print>1,fprintf('distance remaining %g time %g',d(i,j,a,b),t(i,j,a,b));end
                     elseif d(i,j,a,b)>0,
-                        t_end=t(i,j,a,b)+d(i,j,a,b)./ros(i,j,a,b); % time at the end point
-                        if print,fprintf('time at end %g ',t_end);end
+                        t_end=t(i,j,a,b)+dir*d(i,j,a,b)./ros(i,j,a,b); % time at the end point
+                        if print>1,fprintf('time at end %g ',t_end);end
                         ii=i+a-2; % the grid point this end point coincides with 
                         jj=j+b-2;
                         if ii>=1 & ii<=m,if jj>=1 & jj<=n & fire_area(ii,jj)
-                            val=min(t(ii,jj,2,2),t_end);
-                            if print,fprintf('setting %i %i from %g to %g',ii,jj,t(ii,jj,2,2),val);end
+                            val=dir*min(dir*t(ii,jj,2,2),dir*t_end);
+                            if print>1,fprintf('setting %i %i from %g to %g',ii,jj,t(ii,jj,2,2),val);end
                             t(ii,jj,:,:)=val;               % reinitialize the point ii, jj
                             d(ii,jj,:,:)=distance(ii,jj,:,:); % no distance traveled 
                             active(ii,jj)=true;             % can propagate from this
@@ -58,16 +62,15 @@ for step=1:100,
                         d(i,j,a,b)=0;
                     end
                 end
-                if print,fprintf('\n');end
+                if print>1,fprintf('\n');end
             end, end, end
         end
     end, end
-    step,tign=t(:,:,2,2)
+    if print>0,step,tign=t(:,:,2,2),end
     done=~any(t(:)-t_old(:));
     if done,
         break
     end
-    % fclose(h)
 end
 tign=t(:,:,2,2);
 
--- other/Matlab/perimeter_new/propagate_test.m
+++ other/Matlab/perimeter_new/propagate_test.m
@@ -10,9 +10,12 @@ for a=1:3,
         distance(:,:,a,b)=norm([a-2,b-2]);
     end
 end
-time_now=50;
 fire_area=ones(m,n);
 fire_area(4:6,8:11)=0;
+time_now=100;
 tign=propagate(tign,1,fire_area,distance,ros,time_now)
+dir=-1;
+time_now=50*dir;
 tign=100*ones(m,n); tign(:,1)=1; tign(:,end)=1; tign(1,:)=1;tign(end,:)=1;
-tign=propagate(tign,1,fire_area,distance,ros,time_now)
+tign=dir*tign+2;
+tign=propagate(tign,dir,fire_area,distance,ros,time_now)
