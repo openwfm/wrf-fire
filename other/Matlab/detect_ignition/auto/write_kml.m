2f4a1786a45b3f0ef4db3392cab9bd0be0db5048
jhaley <james.haley@ucdenver.edu>
2016-08-11 13:23:12 -0600

    added comments and noted changes needed

--- other/Matlab/detect_ignition/auto/write_kml.m
+++ other/Matlab/detect_ignition/auto/write_kml.m
@@ -1,6 +1,7 @@
 function [ ] = write_kml( in_struct )
 % Function writes a kml file based on input of in_struct, must contain logs
-
+% Use one of the layers in the layers array structure
+% example --> write_kml(layers(1)), layers(1) is the in_struct
 %[m n] = size(in_struct.pts);
 [m n] = size(in_struct.names);
 %open file
@@ -14,6 +15,8 @@ fprintf(fileID,'<Document> \n');
 
 %create icons %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %get an integer from ignition time
+% only works for ignition times which are multiples of 3600% 
+% need to change this
 int_time = (in_struct.time/3600 - 1)/2 +1;
 pins =cellstr(['http://maps.google.com/mapfiles/kml/pushpin/blue-pushpin.png  ';
        'http://maps.google.com/mapfiles/kml/pushpin/grn-pushpin.png   ';
@@ -58,7 +61,8 @@ for i=1:m
     fprintf(fileID,'         <Point> \n');
     fprintf(fileID,'            <extrude>1</extrude> \n');
     fprintf(fileID,'            <altitudeMode>absolute</altitudeMode>\n');
-    %% upside down log --> divide by -10
+    %% set height of pushpin, needs to be changed so that it is not dependent
+	% on this set of data
     fprintf(fileID,'            <coordinates>%4.3f,%4.3f,%5.3f</coordinates> \n',in_struct.pts(i,2),in_struct.pts(i,1),in_struct.logs(i)/10+9000);
     fprintf(fileID,'         </Point> \n');
     fprintf(fileID,'      </Placemark>\n');
