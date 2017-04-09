fec8704e7d22e5c7f0ad0d57d0a8247485a591ea
Jan Mandel <jan.mandel@gmail.com`>
2017-04-09 17:52:37 -0600

    fixing gridinterp

--- other/Matlab/util1_jan/gridinterp.m
+++ other/Matlab/util1_jan/gridinterp.m
@@ -14,21 +14,25 @@ for i=1:mi
     for j=1:ni
         xx=xi(i,j);
         yy=yi(i,j);
-        % find the nearest point
-        d2=(x-xx).*(x-xx)+(y-yy).*(y-yy);
-        k=find(d2(:)==min(d2(:)));k=k(1);
-        ix=ii(k);
-        iy=jj(k);
-        if ix>1 & ix < mm & iy >1 & iy < nn,
-            x3=x(ix-1:ix+1,iy-1:iy+1);
-            y3=y(ix-1:ix+1,iy-1:iy+1);
-            v3=v(ix-1:ix+1,iy-1:iy+1);
-            v_interp = scatteredInterpolant(x3(:),y3(:),v3(:));
-            vv = v_interp(xx,yy);
-            % disp(x3);disp(xx);disp(y3),disp(yy),disp(v3),disp(vv)
-            % hold off; mesh(x3,y3,v3);hold on;plot3(xx,yy,vv,'*k');hold off;drawnow
+        if ~isnan(xx) & ~isnan(yy),
+            % find the nearest point
+            d2=(x-xx).*(x-xx)+(y-yy).*(y-yy);
+            k=find(d2(:)==min(d2(:)));k=k(1);
+            ix=ii(k);
+            iy=jj(k);
+            if ix>1 & ix < mm & iy >1 & iy < nn,
+                x3=x(ix-1:ix+1,iy-1:iy+1);
+                y3=y(ix-1:ix+1,iy-1:iy+1);
+                v3=v(ix-1:ix+1,iy-1:iy+1);
+                v_interp = scatteredInterpolant(x3(:),y3(:),v3(:));
+                vv = v_interp(xx,yy);
+                % disp(x3);disp(xx);disp(y3),disp(yy),disp(v3),disp(vv)
+                % hold off; mesh(x3,y3,v3);hold on;plot3(xx,yy,vv,'*k');hold off;drawnow
+            else
+                vv = NaN;
+            end
         else
-            vv = NaN;
+            vv=NaN;
         end
         vi(i,j)=vv;
     end
