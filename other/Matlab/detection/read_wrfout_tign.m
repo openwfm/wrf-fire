c95c0c5b5f8007dd1d0acdb5ac96240343abf855
Jan Mandel <jan.mandel@gmail.com>
2017-05-24 23:52:23 -0600

    read_wrfout_tign storing times as char

--- other/Matlab/detection/read_wrfout_tign.m
+++ other/Matlab/detection/read_wrfout_tign.m
@@ -7,12 +7,12 @@ function w=read_wrfout_tign(f,ts)
     %    ts time step (string, optional). Read last time step if not given.
     % 
     t=nc2struct(f,{'Times'},{});  nframes=size(t.times,2);
-    times=char(t.times');
-    fprintf('Last time step in %s is %i at %s\n',f,nframes,times(nframes,:))
+    alltimes=char(t.times')
+    fprintf('Last time step in %s is %i at %s\n',f,nframes,alltimes(nframes,:))
     if exist('ts','var')
         frame=0;
         for i=nframes:-1:1,
-            if strcmp(ts,times(i,:))
+            if strcmp(ts,alltimes(i,:))
                 fprintf('Found timestep %i at %s\n',i,ts)
                 frame=i;
                 break
@@ -28,4 +28,9 @@ function w=read_wrfout_tign(f,ts)
     end
     w=nc2struct(f,{'Times','XTIME','TIGN_G','FXLONG','FXLAT','UNIT_FXLAT','UNIT_FXLONG',...
         'XLONG','XLAT','NFUEL_CAT','ITIMESTEP','FIRE_AREA'},{'DX','DY','DT'},frame);
+    w.times=char(w.times');
+    w.cwd=pwd;
+    w.datestr=datestr(clock);
+    s=ls('-l',f);w.ls=s(1:end-1);
+    s=ls('-lL',f);w.lsL=s(1:end-1);
 end
