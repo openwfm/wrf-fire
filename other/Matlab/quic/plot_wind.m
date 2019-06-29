856e3d8ea3a8a4dd0415b21ccaf6e821690df902
Jan Mandel <jan.mandel@gmail.com>
2019-06-29 17:03:43 -0600

    better wind plot

--- other/Matlab/quic/plot_wind.m
+++ other/Matlab/quic/plot_wind.m
@@ -1,22 +1,33 @@
-function plot_wind(u3,h3)
-m=[20,20,10];  % points to display in each direction
+function plot_wind(u3,h3,d)
+% plot_wind(u3,h3,s)
+%   u3  cell array size 3 with the wind components
+%   h3  vector of mesh spacing (m)
+%   d   (optional) distance between points to display, in all 3 directions     
+
+if ~exist('d','var')
+    d = h3(3)/3;
+end
+
 % interpolate to center points
 u = 0.5*(u3{1}(1:end-1,:,:)+u3{1}(2:end,:,:));
 v = 0.5*(u3{2}(:,1:end-1,:)+u3{2}(:,2:end,:));
 w = 0.5*(u3{3}(:,:,1:end-1)+u3{3}(:,:,2:end));
+
+% dimension of the center point array
 n=size(u);
+
 for i=1:3
-    pos{i}=h3(i)*([1:n(i)]-0.5); % vector of positions in dimension i
-    qi{i}=1+[0:m(i)-1]*(n(i)-1)/(m(i)-1);
+    % c{i}=h3(i)*(1/2 + [1:n(1)]);     % coordinates of the centers
+    p{i} = [d/2 : d : h3(i)*(n(i)-1)]; % display points in physical space [0, h3(i)*(n(i)-1)]
+    q{i} = 1+p{i}/h3(i);               % display points in index space [1,n(i)]
 end
-[x,y,z]=ndgrid(pos{1},pos{2},pos{3});
-[ix,iy,iz]=ndgrid(qi{1},qi{2},qi{3});
-xd = interp3(x,ix,iy,iz);
-yd = interp3(y,ix,iy,iz);
-zd = interp3(z,ix,iy,iz);
-ud = interp3(u,ix,iy,iz);
-vd = interp3(v,ix,iy,iz);
-wd = interp3(w,ix,iy,iz);
+% [x,y,z]    = ndgrid(c{1},c{2},c{3});
+[xd,yd,zd] = ndgrid(p{1},p{2},p{3});
+[iq,jq,kq] = ndgrid(q{1},q{2},q{3});
+
+ud = interp3(u,iq,jq,kq);
+vd = interp3(v,iq,jq,kq);
+wd = interp3(w,iq,jq,kq);
 
 quiver3(xd,yd,zd,ud,vd,wd)
 end
