515c08cb1dbf64d77d5546dcb6f9525ec107d2d7
Jan Mandel <jan.mandel@gmail.com>
2019-08-13 15:44:38 -0600

    add_terrain_to_mesh.m makes a hill

--- other/Matlab/quicwind/add_terrain_to_mesh.m
+++ other/Matlab/quicwind/add_terrain_to_mesh.m
@@ -1,8 +1,28 @@
-function XX=add_terrain_to_mesh(X, t, kind)
-check_mesh(X)
-XX=X;
+function XX=add_terrain_to_mesh(X, kind, how)
+check_mesh(X);
+
+x = X{1}(:,:,1); y=X{2}(:,:,1);z=X{3};
 kmax=size(X{1},3);
-switch kind
+if ischar(kind),
+    switch kind
+        case 'hill'
+            cx = mean(x(:));
+            cy = mean(y(:));
+            hz = max(z(:))-min(z(:));
+            rx = mean(abs((x(:)-cx)));
+            ry = mean(abs((y(:)-cy)));
+            a = ((x-cx)./rx).^2 + ((y-cy)./ry).^2 ;
+            t = hz*exp(-a*2)/2;
+        otherwise
+            error('add_terrain_to_mesh: unknown kind')
+    end
+elseif isnumeric(kind),
+    t=kind;
+else
+    error('kind must be string or numeric')
+end
+
+switch how
     case {'shift','s'}
         disp('shifting mesh by terrain vertically')
         XX=X;
@@ -16,7 +36,7 @@ switch kind
             XX{3}(:,:,k)=X{3}(:,:,k)+t*(kmax-k)/(kmax-1);
         end
     otherwise
-        error('add_terrain_to_mesh: unknown kind')
+        error('add_terrain_to_mesh: unknown how')
 end
 check_mesh(XX)
 end
--- other/Matlab/quicwind/mass_cons_flux_test.m
+++ other/Matlab/quicwind/mass_cons_flux_test.m
@@ -1,8 +1,12 @@
 function err = mass_cons_flux_test
 disp('mass_cons_flux_test - testing mass consistent flux approximation with terrain')
-n=[20,30,5];
-h = rand(1,3);
-w = rand(1,3);
+nx=5; ny=3; nz=2;
+
+hh=rand(1,3);
+[x,y,z]   = ndgrid(hh(1)*[0:nx],hh(2)*[0:ny],hh(3)*[0:nz]);
+X={x,y,z};
+X=add_terrain_to_mesh(X,'hill','shift')
+
 lambda0=zeros(n);
 u0= grad3z(rand(n),'zero at bottom');
 warning('not ready')
