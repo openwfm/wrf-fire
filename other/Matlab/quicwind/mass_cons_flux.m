02a516617a21bb8aad2f4c11a18e1d98a0105f2d
Jan Mandel <jan.mandel@gmail.com>
2019-08-12 21:28:29 -0600

    stash

--- other/Matlab/quicwind/mass_cons_flux.m
+++ other/Matlab/quicwind/mass_cons_flux.m
@@ -1,31 +1,35 @@
-function u=mass_cons_flux(u0,h,w,check)
+function u=mass_cons_flux(U0,X,w)
 % mass consistent flux approximation
-% given
-%   u0  3 wind vectors on staggered grids
-%   h   step, vector length 3
-%   w   weights, vector length 3
+% input:
+%   U0  cell array, wind vectors u v w on staggered grids
+%   X   cell array, nodal coordinatees
+%   W   relative weights for u v w, vector length 3
+% output:
+%   U   cell array, wind vectors u v w 
+%
+% using
+%   D = divergence of flux on cell sides
+%   M = transformation of wind to flux through cell sides with
+%       zero flux boundary condition at the bottom (the terrain)
+%   A = weights of the wind vector components, U'*A*U approx ||U||_L2^2
+%    
+% compute  
+%    min 1/2 (U - U0)'*A*(U - U0) s.t. D*M*U = 0
+%
+%    <=>    A*(U - U0) + M'*D'*Lambda = 0
+%           D*M*U                     = 0
+%
+%    <=>    U = U0 - inv(A)*M'*D'*Lambda
+%           where        
+%           D*M*inv(A)*M'*D'*Lambda = D*M*U0 
 
-% solve  
-% min_u 1/2 <D(u-u0),u-u0> s.t. div u = 0, D=diag[w(1)I,w(2)I,w(3)I]
-% <=> min_u max_lambda 1/2 sum_i  <D(u-u0),u-u0> + <lambda,div u> 
-% (using <lambda,div u> = - <grad lambda, u>) 
-% <=> D(u-u0) - grad lambda = 0, div u = 0
-% <=> inv(D) grad lambda = u - u0, div u = 0
-% <=> div inv(D) grad lambda = div u - div u0
-% <=>
-% ______________________________________________________
-%    - div inv(D) grad lambda = div u0,
-%     u = u0 + inv(D) grad lambda
-% with boundary condidions lambda=0 on the sides and top
-% and dlambda/dz = 0 on the bottom
-%-------------------------------------------------------
 
 if ~exist('check','var')
     check = false;
 end
 tstart=tic;
 % divergence of u0
-f = div3(u0,h); 
+f = div3(U0,h); 
 % reflect the right hand side abound the bottom in 3rd coordinate
 n = size(f);
 fprintf('mass_cons_int mesh size %i %i %i\n',n)
@@ -42,11 +46,11 @@ lambda = lambda(:,:,n(3)+1:2*n(3));
 g = grad3z(lambda,h,true);
 % update u
 for i=1:3
-    u{i} = u0{i} + (1/w(i))*g{i};
+    u{i} = U0{i} + (1/w(i))*g{i};
 end
 % check divergence
 if check, err_div = big(div3(u,h)), end
 % check no correction in vertical speed at the bottom
-if check, err_corr_w_bottom = big(u{3}(:,:,1)-u0{3}(:,:,1)), end
+if check, err_corr_w_bottom = big(u{3}(:,:,1)-U0{3}(:,:,1)), end
 fprintf('mass_cons_int %g seconds\n',toc(tstart))
 end
