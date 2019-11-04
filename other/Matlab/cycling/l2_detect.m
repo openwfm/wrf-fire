157cfadc37da96c4e1ae68c894b0e167222a969c
James Haley <james.haley@ucdenver.edu>
2019-11-04 11:14:32 -0700

    made changes to work towards using L2 data

--- other/Matlab/cycling/l2_detect.m
+++ other/Matlab/cycling/l2_detect.m
@@ -1,6 +1,46 @@
-stash = '/home/jhaley/JPSSdata';
+prefix = '/home/jhaley/JPSSdata/';
 
 
-Longitude = hdfread('/home/jhaley/JPSSdata/MOD03.A2013222.0545.006.2013222112442.hdf', 'MODIS_Swath_Type_GEO', 'Fields', 'Longitude');
-Latitude = hdfread('/home/jhaley/JPSSdata/MOD03.A2013222.0545.006.2013222112442.hdf', 'MODIS_Swath_Type_GEO', 'Fields', 'Latitude');
-fire_mask = hdfread('/home/jhaley/JPSSdata/MOD14.A2013222.0545.006.2015263221706.hdf', '/fire mask', 'Index', {[1  1],[1  1],[2030  1354]});
+%Longitude = hdfread('/home/jhaley/JPSSdata/MOD03.A2013222.0545.006.2013222112442.hdf', 'MODIS_Swath_Type_GEO', 'Fields', 'Longitude');
+%Latitude = hdfread('/home/jhaley/JPSSdata/MOD03.A2013222.0545.006.2013222112442.hdf', 'MODIS_Swath_Type_GEO', 'Fields', 'Latitude');
+%fire_mask = hdfread('/home/jhaley/JPSSdata/MOD14.A2013222.0545.006.2015263221706.hdf', '/fire mask', 'Index', {[1  1],[1  1],[2030  1354]});
+
+dhdf=dir([prefix,'*.hdf']);
+dh5 = dir([prefix,'*.h5']);
+dnc = dir([prefix,'*.nc']);
+d=[{dhdf.name},{dh5.name},{dnc.name}];
+%d={d.name};
+
+if(isempty(d)), error(['No files found for ',prefix]),end
+
+% order the files in time
+nfiles=length(d);
+t=zeros(1,nfiles);
+for i=1:nfiles
+    f{i}=[prefix,d{i}];
+    t(i)=rsac2time(d{i});
+end
+
+[t,i]=sort(t);
+p.file={d{i}};
+p.time=t;
+
+fires = [0 0]
+for k = 1:nfiles
+    if mod(k,2) == 1
+        file = p.file{k};
+        file2 = p.file{k+1};
+        v = readl2data(prefix,file,file2);
+        
+        dets = v.data >= 7;
+        v
+        if sum(v.pixels.fire) > 0
+            fires = [fires; v.lons(dets) v.lats(dets)];
+        end
+        
+    end
+end
+
+fires = fires(2:end,:);
+figure,scatter(fires(:,1),fires(:,2))
+
--- other/Matlab/detection/rsac2time.m
+++ other/Matlab/detection/rsac2time.m
@@ -4,8 +4,23 @@ str=regexp(name,'\.[0-9]+\.','match');
 if isempty(str),
     error('NASA time string not found')
 end
-str=str{1};
-str=['20',str(2:end-1)];  %yyyydddHHMMSSS
+% detect level data, file name is not *.mat
+if name(end) ~= 't'
+    %name
+    s1 = regexp(name,'\.[A-0]+\.','match');
+    s1 = s1{1};
+    s1 = s1(3:end-1);
+    s2 = regexp(name,'\.[0-9]+\.','match');
+    s2 = s2{1};
+    s2 = [s2(2:end-1),'00'];
+    str = [s1,s2];
+%read *.tif.mat file
+else
+    str=regexp(name,'\.[0-9]+\.','match');
+    str=str{1};
+    str=['20',str(2:end-1)];  %yyyydddHHMMSSS
+end
+
 t=julian2time(str);
 if nargout>1,
     varargout{1}=str;
