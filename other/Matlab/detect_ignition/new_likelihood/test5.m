bb422e2c8da2648daf3307c1cf7342a3b70366b0
James Haley <james.haley@ucdenver.edu>
2019-11-07 13:38:08 -0700

    updating test5.m, moving detection swath thhrough time.

--- other/Matlab/detect_ignition/new_likelihood/test5.m
+++ other/Matlab/detect_ignition/new_likelihood/test5.m
@@ -1,8 +1,16 @@
+function rs = test5(test,like_spline)
+
 %test5
 close all
 fire_cone = @(x,y) 10*sqrt(( x.^2 + y.^2));
 
-position = linspace(-10,10,201);
+% if ~exist('like_spline')
+%     load spline_data.mat;
+%     [like_spline,fit1] = createFit(t,log_like);
+% end
+g = 200;
+grid_size =2*g+1;
+position = linspace(-10,10,grid_size);
 [x,y]= meshgrid(position,position);
 
 z = fire_cone(x,y);
@@ -11,41 +19,102 @@ fire_top = z > 100;
 z(fire_top) = 100;
 mesh(x,y,z);
 hold on
-scatter3(7,0,49,'r*');
+contour3(x,y,z,[49 49],'k')
+%scatter3(7,0,49,'r*');
 xlabel('x');
-
+hold off
 
 slice_time = [47 49 51];
 fires = zeros(size(x));
+
 %fires(171,100) = 9;
-fires(30:40,30:40) = 5;
+%fires(30:40,30:40) = 5;
 
-tot = 0;
-% for i = 1:3
-%     tot = 0;
-%     input_time = slice_time(i) - z;
-%     temp = like_spline(input_time(fires > 7));
-%     tot = tot + sum(temp);
-%     t1 = like_spline(input_time(fires < 1));
-%     temp = log(1 - exp(1).^t1);
-%     tot = tot + sum(temp)
-% end
-
+%test #1
+if test == 1
+    tot = 0;
+    for i = 1:3
+        tot = 0;
+        input_time = slice_time(i) - z;
+        temp = like_spline(input_time(fires > 7));
+        tot = tot + sum(temp);
+        t1 = like_spline(input_time(fires < 1));
+        temp = log(1 - exp(1).^t1);
+        tot = tot + sum(temp)
+    end
+end %test 1
+% test 2
 % move fire pixel across
-input_time = slice_time(2) - z;
-tots = zeros(1,201);
-for i = 1:201
-    scatter3(x(100,i),y(100,i),slice_time(2),'r*');
-    t_mask = fires;
-    t_mask(100,i) = 9;
-    temp = like_spline(input_time(t_mask > 7));
-    tots(i) = tots(i) + sum(temp);
-    t1 = like_spline(input_time(t_mask < 1));
-    temp = log(1 - exp(1).^t1);
-    tots(i) = tots(i) + sum(temp);
+if test == 2
+    input_time = slice_time(2) - z;
+    tots = zeros(1,grid_size);
+    for i = 1:201
+        scatter3(x(g,i),y(g,i),slice_time(2),'r*');
+        t_mask = fires;
+        t_mask(g,i) = 9;
+        temp = like_spline(input_time(t_mask > 7));
+        tots(i) = tots(i) + sum(temp);
+        t1 = like_spline(input_time(t_mask < 1));
+        temp = log(1 - exp(1).^t1);
+        tots(i) = tots(i) + sum(temp);
+    end
+    hold off
+    figure,plot(x(g,:),tots);
+end %test 2
+
+if test == 3
+    fires = 5*ones(size(x));
+    num_pts = 1000;
+    x_coords = 1+round(2*g*rand(1,num_pts));
+    y_coords = 1+round(2*g*rand(1,num_pts));
+    %figure,scatter(x_coords,y_coords);
+    %make fire mask
+    hold on
+    radius = slice_time(2)/10;
+    angle = linspace(0,2*pi);
+    perim = zeros(length(angle),2);
+    perim(:,1) = radius*cos(angle)';
+    perim(:,2) = radius*sin(angle)';
+    plot(perim(:,1),perim(:,2),'k')
+    
+    for i = 1:num_pts
+        u = x(x_coords(i),y_coords(i));
+        v = y(x_coords(i),y_coords(i));
+        zt = norm([u v]);
+        if abs(zt - radius) < 1 && zt < radius%49
+            fires(x_coords(i),y_coords(i)) = 9;
+            scatter(u,v,'r*');
+        else
+            if rand < 0.99
+                fires(x_coords(i),y_coords(i)) = 0;
+                scatter(u,v,'b');
+            else
+                fires(x_coords(i),y_coords(i)) = 9;
+                scatter(u,v,'r*');
+            end
+            
+        end
+    end
+    
+    num_slices = 21;
+    %times = linspace(min(z(:))+1,max(z(:))-1,num_slices);
+    times = linspace(40,70,num_slices);
+    tots = zeros(size(times));
+    for i = 1:num_slices
+        input_time = times(i) - z;
+        t_mask = fires;
+        temp = like_spline(input_time(t_mask > 7));
+        tots(i) = tots(i) + sum(temp);
+        t1 = like_spline(input_time(t_mask < 1));
+        temp = log(1 - exp(1).^t1);
+        tots(i) = tots(i) + sum(temp);
+    end
+    hold off
+    figure,plot(times,tots)
+    
+end % test 3
+
 end
-hold off
-%figure,plot(input_time(100,:),tots);
     
 
 
