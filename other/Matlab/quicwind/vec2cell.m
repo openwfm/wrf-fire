f65030c11b2076e6f8d4d924da3287aaf227d619
Jan Mandel <jan.mandel@gmail.com>
2019-12-13 10:02:12 -0700

    adding comments

--- other/Matlab/quicwind/vec2cell.m
+++ other/Matlab/quicwind/vec2cell.m
@@ -1,4 +1,17 @@
 function c=vec2cell(v,p)
+% c=vec2cell(v,p)
+% Reshape vector v into cell array of nd-arrays
+% of the same sizes as the template p. The total number of entries
+% in the vector v must equal to the total number of entries
+% in the arrays in p
+%
+% in:
+% v     vector
+% p     cell array
+% out:
+% c     cell array
+%
+
 [m,n]=size(p);
 k=0;
 for j=1:n
