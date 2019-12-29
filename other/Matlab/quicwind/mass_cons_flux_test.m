8712b7c413f96fed5c0ee6fb31054ebc21f1962d
Jan Mandel <jan.mandel@gmail.com>
2019-12-29 01:47:36 -0700

    making mass_cons_flux_test work again, figure ok

--- other/Matlab/quicwind/mass_cons_flux_test.m
+++ other/Matlab/quicwind/mass_cons_flux_test.m
@@ -1,12 +1,12 @@
 function err = mass_cons_flux_test
 disp('mass_cons_flux_test - testing mass consistent flux approximation with terrain')
-nx=10; ny=10; nz=10;
 
 % hh=rand(1,3);
 % hh=[1,1,1];
 % [x,y,z]   = ndgrid(hh(1)*[0:nx],hh(2)*[0:ny],hh(3)*[0:nz]);
 % X={x,y,z};
-mesh_len=[16,16,80];
+mesh_len=[20,20,20];
+mesh_disp=[16,16,16];
 h=rand(1,3);
 X = regular_mesh(mesh_len,h,1.2);
 X=add_terrain_to_mesh(X,'hill','shift',0.3)
@@ -20,9 +20,9 @@ U0{2}=0*ones(size(U0{2}));
 U0{3}=0*ones(size(U0{3}));
 
 [U,Lambda_d] = mass_cons_flux(U0,X,'direct','check'); 
-plot_wind_above_terrain(U,X,mesh_len)
+plot_wind_above_terrain(U,X,mesh_disp)
 [V,Lambda_pcg,err] = mass_cons_flux(U0,X,'pcg','check');
-plot_wind_above_terrain(V,X,mesh_len)
+plot_wind_above_terrain(V,X,mesh_disp)
 direct_vs_pcg = big(cell2vec(U)-cell2vec(V))
 lambda_diff = big(Lambda_d-Lambda_pcg)
 
